271865205af3c3df17ada6802cea69086abcbb61
kalebesampaio <kalebesmp2002@gmail.com>
2024-04-15 13:59:09 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import "express-async-errors";
+import express, { Application, json } from "express";
+import helmet from "helmet";
+
+
+const cors = require("cors");
+
+const app: Application = express();
+app.use(cors());
+app.use(helmet());
+app.use(json());
+
+
+export default app;
