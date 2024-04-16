cdb8da12a495c3eeca270611f2e68c787ddc7dac
kalebesampaio <kalebesmp2002@gmail.com>
2024-04-16 09:57:45 -0300

    feat: criando as entinties

---
+++ src/entities/task.entity.ts
@@ -0,0 +1,26 @@
+import {
+    Column,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+  } from "typeorm";
+  import { User } from "./user.entity";
+  
+  @Entity("tasks")
+  export class Task {
+    @PrimaryGeneratedColumn("increment")
+    id: string;
+  
+    @Column({ length: 125 })
+    title: string;
+  
+    @Column({ nullable: true })
+    description: string;
+  
+    @Column({ default: "false" })
+    completed: boolean;
+  
+    @ManyToOne(() => User)
+    user: User;
+  
+  }
